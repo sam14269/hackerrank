e5db704468d1b1f7e1f342ed8c240211a5e925df
Shatruanjay Kumar <darkspore3725@gmail.com>
2017-07-12 02:17:47 +0530

    Camel Case

---
+++ camelcase.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string s;
+    int cnt=1;
+    cin >> s;
+    for(int i=0;i<s.length();i++)
+        {
+        if(s[i]>=65 && s[i]<=91)
+            cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}
