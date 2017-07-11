8d0b872d86989d0fee4bddae842b4099e8beb9e9
Shatruanjay Kumar <darkspore3725@gmail.com>
2017-07-12 02:09:13 +0530

    Cut the sticks

---
+++ cutSticks.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+int numOfSticks;
+cin >> numOfSticks;
+int stickArray[numOfSticks];
+for(int i = 0; i < numOfSticks;i++){
+cin >> stickArray[i];
+}
+int start = 0;
+int end = numOfSticks;
+
+while(numOfSticks != 0){
+cout << numOfSticks << endl;
+            sort(stickArray, stickArray + end);
+            int cut = stickArray[start];
+            for(int i = start; i < end;i++){
+            if(stickArray[i] - cut <= 0){
+            start++;
+            numOfSticks--;
+            }
+            stickArray[i] -= cut;
+            //cout << stickArray[i];
+            }
+}
+
+return 0;
+}
